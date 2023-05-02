36f12a2050d67d57bdfaba452ce0317334d5b378
Mohit-Aasirwal <mohitaasirwal@gmail.com>
2023-05-02 16:48:13 +0530

    files and folders

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,26 @@
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
-const inter = Inter({ subsets: ["latin"] });
+import Landing from "@/components/Landing";
+import About from "@/components/About";
+import Events from "@/components/Events";
+import Achievements from "@/components/Achievements";
+import Contact from "@/components/Contact";
+// const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col w-full items-center justify-between p-24">
-      <div className="bg-black flex flex-col w-full items-center justify-center h-screen">
-        <h1 className="text-3xl font-bold mb-4">Under Development</h1>
-        <p className="text-gray-600 text-lg mb-8">
+    <main className="flex max-h-screen flex-col w-full items-center justify-between p-24">
+      <div className="bg-gray-800 flex flex-col w-full items-center justify-center h-screen">
+        <h1 className="text-6xl font-bold mb-4">Under Development</h1>
+        <p className="text-white text-lg mb-8">
           We&apos;re currently working on this website. Stay tuned!
         </p>
-      <Navbar />
+        {/* <Navbar /> */}
+        {/* <Landing/> */}
+        {/* <About/> */}
+        {/* <Events/> */}
+        {/* <Achievements/> */}
+        {/* <Contact/> */}
       </div>
     </main>
   );
