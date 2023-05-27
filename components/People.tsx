5a61a05da843748c714cb770fdf0926c8c40cc2d
Mohit-Aasirwal <mohitaasirwal@gmail.com>
2023-05-27 11:40:36 +0530

    people half done

--- components/People.tsx
+++ components/People.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+//pending
+import Image from "next/image";
+
+import React from "react";
 
 const People = () => {
   return (
-    <div>People</div>
-  )
-}
+    <div className="bg-[#00C2FF] mx-10 rounded-3xl">
+      <h1 className="text-4xl text-white font-bold px-16 pt-10">Our Team</h1>
+      <p className="text-white px-16 py-5">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum,
+        minus!
+      </p>
+      
+    </div>
+  );
+};
 
-export default People
+export default People;
