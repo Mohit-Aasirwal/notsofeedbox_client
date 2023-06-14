670e83ca76b0ea3d0f2164b20c2953aac7ad00d8
Mustafa_Azad <21bcs038@ietdavv.edu.in>
2023-06-14 16:58:57 +0530

    Function for fetching data from backend implemented with typescript

--- components/Events/UpcomingEve.tsx
+++ components/Events/UpcomingEve.tsx
@@ -1,45 +1,59 @@
 import Image from "next/image";
 import React from "react";
 import Lists from "./Lists";
-import { motion } from "framer-motion";
+import { EventSec } from "@/types";
 
-function UpcomingEve() {
-  return (
-    <>
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 5 }}
-        className="grid md:grid-cols-2 grid-cols-1 md:h-[70vh] h-full"
-        id="Events"
-      >
-        <div className="flex md:space-x-8 md:mx-20 space-x-5 items-center">
-          <Image
-            src="/calendar.svg"
-            width={39}
-            height={37}
-            className="xl:w-[49px] xl:h-[47px]"
-            alt="calendar"
-          />
-          <Image
-            src="/event.svg"
-            alt="Upcoming Event"
-            className="lg:w-[300px] lg:h-[300px] w-60 h-60"
-            width={300}
-            height={300}
-          />
-        </div>
-        <div className="h-full space-y-8 flex flex-col items-center justify-center">
-          <div className="md:h-[80%] h-full w-full overflow-x-hidden overflow-y-scroll">
-            <Lists />
-            <Lists />
-            <Lists />
-            <Lists />
-          </div>
-        </div>
-      </motion.div>
-    </>
-  );
+interface eve {
+	events: EventSec[];
+}
+
+interface item {
+	attributes: {
+		title_event: string;
+		location: string;
+		date: string;
+		month: string;
+	};
+}
+
+function UpcomingEve({ events }: eve) {
+	return (
+		<>
+			<div className="grid md:grid-cols-2 grid-cols-1 md:h-[70vh] h-full">
+				<div className="flex lg:space-x-8 space-x-5 justify-center items-center">
+					<Image
+						src="/calander.svg"
+						width={39}
+						height={37}
+						className="xl:w-[49px] xl:h-[47px]"
+						alt="calander"
+					/>
+					<Image
+						src="/event.svg"
+						alt="Upcoming Event"
+						className="lg:w-[300px] lg:h-[300px] w-60 h-60"
+						width={300}
+						height={300}
+					/>
+				</div>
+				<div className="h-full  space-y-8 flex flex-col items-center justify-center">
+					<div className="md:h-[60%] h-full overflow-y-scroll">
+						{events.map((item: item, i: number) => {
+							return (
+								<Lists
+									key={i}
+									title={item.attributes.title_event}
+									location={item.attributes.location}
+									date={item.attributes.date}
+									month={item.attributes.month}
+								/>
+							);
+						})}
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default UpcomingEve;
--- components/Stats.tsx
+++ components/Stats.tsx
@@ -1,92 +1,97 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { StatSec, layout } from "@/types";
 
 const animation = {
-  initial: {
-    opacity: 0,
-  },
-  whileInView: {
-    opacity: 1,
-    transition: {
-      duration: 1,
-    },
-  },
+	initial: {
+		opacity: 0,
+	},
+	whileInView: {
+		opacity: 1,
+		transition: {
+			duration: 1,
+		},
+	},
 };
 
-const Stats = () => {
-  return (
-    <motion.div
-      variants={animation}
-      initial="initial"
-      whileInView="whileInView"
-      className="grid md:grid-cols-4 grid-cols-2 my-10 p-10 overflow-hidden"
-    >
-      <motion.h1
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1, scale: 1 }}
-        whileHover={{ scale: 1.2 }}
-        // whileInView={{ opacity: 1, scale: 1 }}
-        transition={{
-          duration: 0.1,
-          type: "spring",
-          stiffness: 1000,
-        }}
-        className="font-poppins md:text-5xl text-3xl text-center text-[#00C2FF]"
-      >
-        5+ <br />{" "}
-        <p className="font-poppins text-[#003865] text-lg font-extralight">
-          Branches
-        </p>
-      </motion.h1>
-      <motion.h1
-        className="font-poppins md:text-5xl text-3xl text-center text-[#00C2FF]"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1, scale: 1 }}
-        whileHover={{ scale: 1.2 }}
-        transition={{
-          duration: 0.1,
-          type: "spring",
-          stiffness: 1000,
-        }}
-      >
-        200+
-        <br />{" "}
-        <p className="font-poppins text-[#003865] text-lg font-extralight">
-          Branches
-        </p>
-      </motion.h1>
-      <motion.h1
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1, scale: 1 }}
-        whileHover={{ scale: 1.2 }}
-        transition={{
-          duration: 0.1,
-          type: "spring",
-          stiffness: 1000,
-        }}
-        className="font-poppins md:text-5xl text-3xl text-center text-[#00C2FF]"
-      >
-        45+
-        <br />{" "}
-        <p className="font-poppins text-[#003865] text-lg font-extralight">
-          Branches
-        </p>
-      </motion.h1>
-      <motion.h1
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1, scale: 1 }}
-        whileHover={{ scale: 1.2 }}
-        transition={{ duration: 0.1, type: "spring", stiffness: 1000 }}
-        className="font-poppins md:text-5xl text-3xl text-center text-[#00C2FF]"
-      >
-        50+
-        <br />{" "}
-        <p className="font-poppins text-[#003865] text-lg font-extralight">
-          Branches
-        </p>{" "}
-      </motion.h1>
-    </motion.div>
-  );
+interface stats {
+	stat: StatSec[];
+}
+
+const Stats = ({ stat }: stats) => {
+	return (
+		<motion.div
+			variants={animation}
+			initial="initial"
+			whileInView="whileInView"
+			className="grid md:grid-cols-4 grid-cols-2 my-10 p-10 overflow-hidden"
+		>
+			<motion.h1
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1, scale: 1 }}
+				whileHover={{ scale: 1.2 }}
+				// whileInView={{ opacity: 1, scale: 1 }}
+				transition={{
+					duration: 0.1,
+					type: "spring",
+					stiffness: 1000,
+				}}
+				className="font-poppins md:text-5xl text-3xl text-center text-[#00C2FF]"
+			>
+				5+ <br />{" "}
+				<p className="font-poppins text-[#003865] text-lg font-extralight">
+					Branches
+				</p>
+			</motion.h1>
+			<motion.h1
+				className="font-poppins md:text-5xl text-3xl text-center text-[#00C2FF]"
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1, scale: 1 }}
+				whileHover={{ scale: 1.2 }}
+				transition={{
+					duration: 0.1,
+					type: "spring",
+					stiffness: 1000,
+				}}
+			>
+				200+
+				<br />{" "}
+				<p className="font-poppins text-[#003865] text-lg font-extralight">
+					Branches
+				</p>
+			</motion.h1>
+			<motion.h1
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1, scale: 1 }}
+				whileHover={{ scale: 1.2 }}
+				transition={{
+					duration: 0.1,
+					type: "spring",
+					stiffness: 1000,
+				}}
+				className="font-poppins md:text-5xl text-3xl text-center text-[#00C2FF]"
+			>
+				45+
+				<br />{" "}
+				<p className="font-poppins text-[#003865] text-lg font-extralight">
+					Branches
+				</p>
+			</motion.h1>
+			<motion.h1
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1, scale: 1 }}
+				whileHover={{ scale: 1.2 }}
+				transition={{ duration: 0.1, type: "spring", stiffness: 1000 }}
+				className="font-poppins md:text-5xl text-3xl text-center text-[#00C2FF]"
+			>
+				50+
+				<br />{" "}
+				<p className="font-poppins text-[#003865] text-lg font-extralight">
+					Branches
+				</p>{" "}
+			</motion.h1>
+		</motion.div>
+	);
 };
 
 export default Stats;
