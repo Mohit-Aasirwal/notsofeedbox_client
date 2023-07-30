663c3511a4cd8b33a4304486889dc478d1072285
Mustafa_Azad <21bcs038@ietdavv.edu.in>
2023-07-30 18:33:38 +0530

    Next Api for all component build

--- api/index.ts
+++ api/index.ts
@@ -1,44 +1,44 @@
 import axios from "axios";
 
-const api = axios.create({
-	baseURL: process.env.API_BASE_URL,
-	headers: {
-		Authorization: `Bearer ${process.env.BACKEND_API_KEY}`,
-	},
-});
+// const api = axios.create({
+// 	baseURL: process.env.NEXT_PUBLIC_BACKEND_API_KEY,
+// 	headers: {
+// 		Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_BASE_URL}`,
+// 	},
+// });
 
 // Hero section
 export const getHero = async () => {
-	const response = await api.get("/api/heroes?populate=*");
+	const response = await axios.get("/api/heros?populate=*");
 	return response.data;
 };
 
 // Stats section
 export const getStats = async () => {
-	const response = await api.get("/api/stats");
+	const response = await axios.get("/api/stats");
 	return response.data;
 };
 
 // Timeline section
 export const getTimeline = async () => {
-	const response = await api.get("/api/timelines");
+	const response = await axios.get("/api/timelines");
 	return response.data;
 };
 
 // Achievement section
 export const getAchievements = async () => {
-	const response = await api.get("/api/achievements?populate=*");
+	const response = await axios.get("/api/achievements?populate=*");
 	return response.data;
 };
 
 // Events section
 export const getEvents = async () => {
-	const response = await api.get("/api/events");
+	const response = await axios.get("/api/events");
 	return response.data;
 };
 
 // Team section
 export const getTeam = async () => {
-	const response = await api.get("/api/teams?populate=*");
+	const response = await axios.get("/api/teams?populate=*");
 	return response.data;
 };
---
+++ pages/api/teams.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function teams(req: NextApiRequest, res: NextApiResponse) {
+	try {
+		const apiUrl =
+			process.env.NEXT_PUBLIC_API_BASE_URL + "/api/teams?populate=*";
+		const response: any = await axios.get(apiUrl, {
+			headers: {
+				Authorization: `Bearer ${process.env.NEXT_PUBLIC_BACKEND_API_KEY}`,
+			},
+		});
+		res.status(200).json(response.data);
+	} catch (error) {
+		console.error(error);
+	}
+}
