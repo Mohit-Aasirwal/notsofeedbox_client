048a78e447d8f694aa2e4d57c5ba4e9c82288078
Mustafa_Azad <21bcs038@ietdavv.edu.in>
2023-07-30 18:59:01 +0530

    Scrollbar Hide and Default added and Image Issue solved

--- components/People.tsx
+++ components/People.tsx
@@ -5,60 +5,60 @@ import { motion } from "framer-motion";
 import { TeamSec } from "@/types";
 
 interface Props {
-  team: TeamSec[];
+	team: TeamSec[];
 }
 
-const team = [{}, {}, {}, {}];
+const team = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}];
 
 const People = () => {
-  // console.log(team);
-  return (
-    <motion.div
-      initial={{ opacity: 0, scale: 0.5 }}
-      animate={{ opacity: 1, scale: 1 }}
-      transition={{
-        duration: 1,
-        delay: 0.5,
-      }}
-      className="bg-[#00C2FF] mx-10 rounded-3xl"
-      id="People"
-    >
-      <h1 className="text-4xl text-white font-bold px-16 pt-10">Our Team</h1>
-      <p className="text-white px-16 py-5">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum,
-        minus!
-      </p>
-      <div className="grid grid-flow-col overflow-x-scroll space-x-10 my-10">
-        {team &&
-          team.map((value: any, id: number) => {
-            return (
-              <motion.div
-                initial={{ opacity: 0.4, scale: 0.6 }}
-                animate={{ opacity: 1, scale: 1 }}
-                transition={{
-                  duration: 1,
-                  delay: 0.5,
-                }}
-                key={id}
-                className="rounded-full ml-20 w-40 h-40 bg-white"
-              >
-                <Image
-                  src={
-                    "/group.png"
-                    // process.env.NEXT_PUBLIC_API_BASE_URL +
-                    // team[0].attributes.img.data.attributes.url
-                  }
-                  alt="images"
-                  width={100}
-                  height={100}
-                  className="rounded-full object-contain"
-                />
-              </motion.div>
-            );
-          })}
-      </div>
-    </motion.div>
-  );
+	// console.log(team);
+	return (
+		<motion.div
+			initial={{ opacity: 0, scale: 0.5 }}
+			animate={{ opacity: 1, scale: 1 }}
+			transition={{
+				duration: 1,
+				delay: 0.5,
+			}}
+			className="bg-[#00C2FF] mx-10 pb-10 mb-8 rounded-3xl"
+			id="People"
+		>
+			<h1 className="text-4xl text-white font-bold px-16 pt-10">Our Team</h1>
+			<p className="text-white px-16 py-5">
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum,
+				minus!
+			</p>
+			<div className="grid grid-flow-col overflow-x-scroll scrollbar-hide my-10">
+				{team &&
+					team.map((value: any, id: number) => {
+						return (
+							<motion.div
+								initial={{ opacity: 0.4, scale: 0.6 }}
+								animate={{ opacity: 1, scale: 1 }}
+								transition={{
+									duration: 1,
+									delay: 0.5,
+								}}
+								key={id}
+								className="rounded-full ml-20 w-40 h-40 bg-white"
+							>
+								<Image
+									src={
+										"/group.png"
+										// process.env.NEXT_PUBLIC_API_BASE_URL +
+										// team[0].attributes.img.data.attributes.url
+									}
+									alt="images"
+									width={800}
+									height={800}
+									className="rounded-full object-cover w-full h-full"
+								/>
+							</motion.div>
+						);
+					})}
+			</div>
+		</motion.div>
+	);
 };
 
 export default People;
